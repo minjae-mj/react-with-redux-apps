95eec0c0ed47dd33c712844f927494c9fab10d8e
minjae-mj <65207835+minjae-mj@users.noreply.github.com>
2021-02-19 22:22:58 +0900

    add loading spinner and styling

--- seasons/src/SeasonDisplay.js
+++ seasons/src/SeasonDisplay.js
@@ -1,37 +1,36 @@
-import React from "react";
+import './SeasonDisplay.css'; 
+import React from 'react';
 
 const seasonConfig = {
-  summer: {
-    text: "Let's hit the beach!",
-    iconName: "sun",
-  },
-  winter: {
-    text: "Burr, it is chilly!",
-    iconName: "snowflake",
-  },
+    summer: {
+        text: "Let's hit the beach!",
+        iconName: 'sun'
+    },
+    winter: {
+        text: "Burr, it's chilly!",
+        iconName: 'snowflake'
+    }
 };
 
 const getSeason = (lat, month) => {
-  if (month > 2 && month < 9) {
-    // northern hemisphere
-    return lat > 0 ? "summer" : "winter";
-  } else {
-    // southern hemisphere
-    return lat > 0 ? "winter" : "summer";
-  }
-};
+    if(month > 2 && month < 9) {
+        return lat > 0 ? 'summer' : 'winter'; 
+    } else {
+        return lat > 0 ? 'winter' : 'summer'; 
+    }
+}; 
 
 const SeasonDisplay = (props) => {
-  const season = getSeason(props.lat, new Date().getMonth());
-  const { text, iconName } = seasonConfig[season];
-
-  return (
-    <div>
-      <i className={`${iconName}`} />
-      <h1>{text}</h1>
-      <i className={`${iconName}`} />
+    const season = getSeason(props.lat, new Date().getMonth()); 
+    const {text, iconName} = seasonConfig[season]; 
+    
+return (
+    <div className={`season-display ${season}`}>
+        <i className={`icon-left massive icon ${iconName}`} />
+        <h1>{text}</h1>
+        <i className={`icon-right massive icon ${iconName}`} />
     </div>
-  );
+    ); 
 };
 
 export default SeasonDisplay;
--- seasons/src/index.js
+++ seasons/src/index.js
@@ -1,32 +1,38 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import SeasonDisplay from "./SeasonDisplay";
+import React from 'react';
+import ReactDOM from 'react-dom'; 
+import SeasonDisplay from './SeasonDisplay'; 
+import Spinner from './Spinner'; 
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
+    // Babel will handle below and create constructor codes behind the scences. 
+    state = { lat: null, errorMessage: '' }; 
 
-    this.state = { lat: null, errMessage: "" };
-  }
-
-  componentDidMount() {
-    window.navigator.geolocation.getCurrentPosition(
-      (position) => this.setState({ lat: position.coords.latitude }),
-      (err) => this.setState({ errMessage: err.message })
-    );
-  }
-
-  render() {
-    if (this.state.errMessage && !this.state.lat) {
-      return <div>Error: {this.state.errMessage}</div>;
+    componentDidMount() {
+        window.navigator.geolocation.getCurrentPosition(
+            (position) => this.setState({ lat: position.coords.latitude }), 
+            (err) => this.setState({errorMessage: err.message})
+        );
     }
 
-    if (!this.state.errMessage && this.state.lat) {
-      return <SeasonDisplay lat={this.state.lat} />;
+    renderContent() {
+        if (this.state.errorMessage && !this.state.lat) {
+            return <div> Error: {this.state.errorMessage} </div> 
+        } 
+
+        if (!this.state.errorMessage && this.state.lat) {
+            return <SeasonDisplay lat={this.state.lat} />
+        }
+        
+        return <Spinner message='Please accept location request' />
     }
 
-    return <div>Loading!</div>;
-  }
-}
+    render() {
+        return (
+            <div>
+                {this.renderContent()}
+            </div>
+        );
+    }; 
+}; 
 
-ReactDOM.render(<App />, document.querySelector("#root"));
+ReactDOM.render(<App />, document.querySelector('#root')); 
