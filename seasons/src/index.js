589e528e2af2dfa2e6a140ea6049c4a879e104ac
Minjae Kwon <dearmin27@gmail.com>
2021-02-19 16:17:40 +0900

    implement conditional rendering based on result of geolocation api

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -1,13 +1,23 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-const App = () => {
-  window.navigator.geolocation.getCurrentPosition(
-    (position) => console.log(position),
-    (err) => console.log(err)
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
 
-  return <div>Hi there!</div>;
-};
+    this.state = { lat: null };
+
+    window.navigator.geolocation.getCurrentPosition(
+      (position) => {
+        this.setState({ lat: position.coords.latitude });
+      },
+      (err) => console.log(err)
+    );
+  }
+
+  render() {
+    return <div>Latitude: {this.state.lat}</div>;
+  }
+}
 
 ReactDOM.render(<App />, document.querySelector("#root"));
